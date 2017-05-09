1eec075f5f5c6cbe46e34acd09f2b70e4076e49d
Slavchev,Veselin V.R <v.slavchev@student.fontys.nl>
2017-05-09 22:01:36 +0200

    Parentheses balancing

---
+++ src/test/scala/Test100.scala
@@ -0,0 +1,32 @@
+import org.scalatest.FunSuite
+
+class Test100 extends FunSuite {
+
+  test("open close should be balanced") {
+    assert(P100.isBalanced("()"))
+  }
+
+  test("empty string should be balanced") {
+    assert(P100.isBalanced(""))
+  }
+
+  test("closing before opening should not be balanced") {
+    assert( ! P100.isBalanced(")("))
+  }
+
+  test("closing more than opening should not be balanced") {
+    assert( ! P100.isBalanced("())))"))
+  }
+
+  test("opening more than closing should not be balanced") {
+    assert( ! P100.isBalanced("(((()"))
+  }
+
+  test("only opening should not be balanced") {
+    assert( ! P100.isBalanced("("))
+  }
+
+  test("only closing should not be balanced") {
+    assert( ! P100.isBalanced(")"))
+  }
+}
